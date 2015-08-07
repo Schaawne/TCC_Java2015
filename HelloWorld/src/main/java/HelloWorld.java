c08059e71d3725ae3be2f95845e7950061d63dd9
Schaawne <sfblanton@gmail.com>
2015-08-06 20:36:44 -0500

    --Set compiler version to Java 1.8 --HelloWorld class created

---
+++ HelloWorld/src/main/java/HelloWorld.java
@@ -0,0 +1,11 @@
+/**
+ * HelloWorld Class
+ *
+ * @author Sean Blanton
+ * @version 0.0.0.1
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
