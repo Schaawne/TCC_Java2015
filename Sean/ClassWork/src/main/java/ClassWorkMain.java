010729bee65838777d4bfe4834697514071368ba
Schaawne <sfblanton@gmail.com>
2015-08-11 19:07:55 -0500

    MongoTinkering Updates

---
+++ Sean/ClassWork/src/main/java/ClassWorkMain.java
@@ -0,0 +1,15 @@
+/**
+ * ClassWorkMain class for ClassWork
+ * <p>
+ * Created by Schaawne on 8/11/2015.
+ *
+ * @author Sean Blanton
+ * @version 0.0.0.1
+ * @since 0.0.0.1
+ */
+public class ClassWorkMain {
+    public static void main(String[] args)
+    {
+
+    }
+}
