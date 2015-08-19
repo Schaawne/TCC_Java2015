42aacfec2abbfce57dc0ef87c113fe62ecce1fa8
Schaawne <sfblanton@gmail.com>
2015-08-19 18:46:43 -0500

    Project01 Initial Checkin

---
+++ Sean/project01Blanton/src/com/IntroJava2015/project01BlantonMain.java
@@ -0,0 +1,22 @@
+package com.IntroJava2015;
+
+/**
+ * project01BlantonMain class for project01Blanton
+ * <p>
+ * Created by Schaawne on 8/19/2015.
+ *
+ * @author Sean Blanton
+ * @version 0.0.0.1
+ * @since 0.0.0.1
+ */
+public class project01BlantonMain {
+    /**
+     * Main method for project01Blanton
+     *
+     * @param args command line parameters
+     */
+    public static void  main(String[] args)
+    {
+        System.out.println("Hello World");
+    }
+}
