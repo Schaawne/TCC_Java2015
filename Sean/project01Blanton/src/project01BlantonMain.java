acbe54e7d51c849a4ad5adfdae3ec30c32728c43
Schaawne <sfblanton@gmail.com>
2015-08-19 19:47:40 -0500

    Project01 working

--- Sean/project01Blanton/src/project01BlantonMain.java
+++ Sean/project01Blanton/src/project01BlantonMain.java
@@ -1,15 +1,16 @@
-package com.IntroJava2015;
-
 /**
  * project01BlantonMain class for project01Blanton
  * <p>
- * Created by Schaawne on 8/19/2015.
+ * Class: Introduction to Java
+ * Project: Project 01
+ * DueDate: 2015.09.03
  *
  * @author Sean Blanton
  * @version 0.0.0.1
  * @since 0.0.0.1
  */
-public class project01BlantonMain {
+public class project01BlantonMain
+{
     /**
      * Main method for project01Blanton
      *
