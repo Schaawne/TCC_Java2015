b4ab1153762e1fbd8ad649af3cc3baff5a183d44
Schaawne <sfblanton@gmail.com>
2015-09-02 19:27:25 -0500

    Commit classwork testing with Scanner to make number guess application

--- Sean/ClassWork/src/main/java/ClassWorkMain.java
+++ Sean/ClassWork/src/main/java/ClassWorkMain.java
@@ -1,3 +1,6 @@
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  * ClassWorkMain class for ClassWork
  * <p>
@@ -10,6 +13,33 @@
 public class ClassWorkMain {
     public static void main(String[] args)
     {
+        Scanner theScanner = new Scanner(System.in);
 
+        Random randomer = new Random();
+        int userGuess, myNumber, topEnd, maxGuesses;
+
+        topEnd = randomer.nextInt(100);
+        myNumber = randomer.nextInt(topEnd) + 1;
+        maxGuesses = myNumber / 3;
+
+        System.out.println("Random number between 1 and " + topEnd + "!\r\nYou have " + maxGuesses + " attempts to guess correctly");
+        for(int i = 0; i < maxGuesses; i++)
+        {
+            System.out.println("Make a guess: ");
+            userGuess = theScanner.nextInt();
+            if(myNumber > userGuess)
+            {
+                System.out.println("Too LOW!!");
+            }
+            else if(myNumber < userGuess)
+            {
+                System.out.println("Too HIGH!");
+            }
+            else
+            {
+                System.out.println("You did it! [" + (i + 1) + " of " + maxGuesses + " guesses used]");
+                break;
+            }
+        }
     }
 }
